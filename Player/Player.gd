c5d8de9a514a1007cb801c87db6390cceac7a8cf
jrosebr <jrosebr@iu.edu>
2023-12-08 18:47:53 -0500

    Finished Basic Solutions Part 1

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,10 +3,21 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.001
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		$Pivot.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -0.5, 0.25)
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,10 +27,16 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("shoot"):
+		var weapons = $Pivot/Weapon
+		for w in weapons.get_children():
+			if w.has_method("shoot"):
+				w.shoot()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
---
+++ Player/blaster.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+
+func shoot():
+	$Muzzle.show()
+	$Timer.start()
+
+func _on_timer_timeout():
+	$Muzzle.hide()
