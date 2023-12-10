02a63b08798fd8130b6d985de2f9b0795b0cc3d1
jrosebr <jrosebr@iu.edu>
2023-12-10 14:57:25 -0500

    Update

--- Player/blaster.gd
+++ Player/blaster.gd
@@ -4,6 +4,11 @@ extends Node3D
 func shoot():
 	$Muzzle.show()
 	$Timer.start()
+	
+	if $Aim.is_colliding():
+		var target = $Aim.get_collider()
+		if target.has_method("damage"):
+			target.damage()
 
 func _on_timer_timeout():
 	$Muzzle.hide()
